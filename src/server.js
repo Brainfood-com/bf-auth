3b0c63d7d10898f11aa8c11400651b32ff908398
Adam Heath <doogie@brainfood.com>
2019-04-01 12:28:00 -0500

    Add cors and session support.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
+import cors from 'cors'
 import express from 'express'
 import Http from 'http'
+import session from 'express-session'
+import SessionFileStore from 'session-file-store'
 
 const app = express()
+app.use(cors({
+  credentials: true,
+  origin: true,
+}))
+app.use(session({
+  cookie: {
+    domain: process.env.AUTH_COOKIE_DOMAIN,
+  },
+  name: process.env.AUTH_COOKIE_NAME,
+  proxy: true,
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.AUTH_SECRET,
+  store: new (SessionFileStore(session))({
+    ttl: parseInt(process.env.AUTH_SESSION_TTL),
+    path: process.env.AUTH_SESSION_PATH,
+  })
+}))
 
 const http = Http.Server(app)
 http.listen(8080, () => {
